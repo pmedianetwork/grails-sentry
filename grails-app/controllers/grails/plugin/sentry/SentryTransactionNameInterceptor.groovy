67a73ea632ce50b8fc8ee15bde82bbdcb0a2e152
Jaroslav Kostal <jaroslav.kostal@adverity.com>
2022-06-29 09:52:08 +0200

    Rename SentryTracingInterceptor to SentryTransactionNameInterceptor

--- grails-app/controllers/grails/plugin/sentry/SentryTransactionNameInterceptor.groovy
+++ grails-app/controllers/grails/plugin/sentry/SentryTransactionNameInterceptor.groovy
@@ -2,14 +2,14 @@ package grails.plugin.sentry
 
 import org.springframework.web.servlet.HandlerMapping
 
-class SentryTracingInterceptor {
+class SentryTransactionNameInterceptor {
 
-    SentryTracingInterceptor() {
+    SentryTransactionNameInterceptor() {
         match(controller: '*')
     }
 
     /**
-     * Sets the request attribute that is used by {@link io.sentry.spring.tracing.TransactionNameProvider TransactionNameProvider} as sentry transaction name.
+     * Sets the request attribute that is used by {@link io.sentry.spring.tracing.SpringMvcTransactionNameProvider SpringMvcTransactionNameProvider} as sentry transaction name.
      * Without it transaction wouldn't be send to sentry by {@link io.sentry.spring.tracing.SentryTracingFilter SentryTracingFilter}.
      */
     boolean before() {
